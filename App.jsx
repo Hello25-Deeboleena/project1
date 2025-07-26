3b8bb8a7ab14c7d8cc39dfb555643d15a0b6b138
Deboleena Paul <deboleenapaul2005@gmail.com>
2025-07-26 14:53:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css'
+// import First from './components/First';
+// import Second from './components/Second';
+// import Third from './components/Third';
+
+// import Division from './components/chessboard';
+import Color from './components/color'
+// import Fourth from './components/Fourth';
+// import Fifth from './components/Fifth';
+// import StudentArray from './components/StudentArray';
+
+function App() {
+
+  return (
+    <>
+      {/* <First/> */}
+      {/* <Second/> */}
+      {/* <Third/> */}
+      {/* <Fourth/>
+      <Fifth/>  */}
+      {/* <Division /> */}
+      <Color />
+    </>
+  )
+}
+
+
+export default App
